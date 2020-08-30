16721e8628d3cd5b747c9ad88d2ddf5fbd623686
Alexandru <rammazzoti2000@gmail.com>
2020-08-30 23:34:05 +0200

    react: Implement useHistory hook to Pokemon component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
-const Pokemon = ({ pokemon }) => (
-  <div>
-    <h1>{pokemon.name}</h1>
+function Pokemon({ pokemon }) {
+  const history = useHistory();
+  const handlePrevious = () => {
+    history.goBack();
+  };
+
+  return (
     <div>
-      <h2>Abilities:</h2>
-      <ul className="pokemon-details">
-        <li>Thunderbolt</li>
-      </ul>
+      <div className="pokemon-container">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <h1>{pokemon.name}</h1>
+      </div>
+
+      <div>
+        <h2>Abilities:</h2>
+        <ul className="stats">
+          {pokemon.abilities.map(item => <li key={item.ability.name}>{item.ability.name}</li>)}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Stats</h2>
+        <ul>
+          {pokemon.stats.map(stat => (
+            <li key={stat.stat.name}>
+              <span>
+                {stat.stat.name}
+                :
+              </span>
+              {stat.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button type="button" onClick={handlePrevious}>Home</button>
     </div>
-  </div>
-);
+  );
+}
 
 export default Pokemon;
