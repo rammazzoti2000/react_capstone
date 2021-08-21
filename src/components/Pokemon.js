6defcda6ddafd0009e91d900a07ea9af25e17a69
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2021-08-21 12:53:49 +0530

    stats visualization

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useHistory } from 'react-router';
-import PropTypes from 'prop-types';
-import picture from '../assets/picture.png';
+import React from "react";
+import { useHistory } from "react-router";
+import PropTypes from "prop-types";
+import picture from "../assets/picture.png";
+import Chart from "react-google-charts";
 
 const Pokemon = ({ pokemon }) => {
   const history = useHistory();
@@ -10,33 +11,109 @@ const Pokemon = ({ pokemon }) => {
     history.goBack();
   };
 
+  const pieOptions = {
+    title: "",
+    pieHole: 0.6,
+    slices: [
+      {
+        color: "#2BB673",
+      },
+      {
+        color: "#d91e48",
+      },
+      {
+        color: "#007fad",
+      },
+      {
+        color: "#e9a227",
+      },
+      {
+        color: "#E38627",
+      },
+      {
+        color: "#C13C37",
+      },
+      {
+        color: "#6A2135",
+      },
+    ],
+    backgroundColor: "none",
+    legend: {
+      position: "labeled",
+      alignment: "center",
+      textStyle: {
+        color: "233238",
+        fontSize: 14,
+      },
+    },
+    pieSliceText: "value",
+    tooltip: {
+      showColorCode: true,
+    },
+    chartArea: {
+      left: 0,
+      top: 0,
+      width: "100%",
+      height: "100%",
+    },
+    fontName: "Roboto",
+  };
+
+  const buildData = (pokemon) => {
+    const array = [["name", "base_stat"]];
+    pokemon.stats.map((stats) =>
+      array.push([stats.stat.name, stats.base_stat])
+    );
+    return array;
+  };
+
   return (
     <div className="container pokemon-detailed max-w-sm m-2 shadow-lg overflow-hidden rounded-md d-flex p-5 justify-content-center">
       <div className="pokemon-container w-25 d-flex ">
-        <img src={pokemon.sprites.front_default ? pokemon.sprites.front_default : picture} alt={pokemon.name} className="pokemon-pic w-100 d-block m-auto" />
+        <img
+          src={
+            pokemon.sprites.front_default
+              ? pokemon.sprites.front_default
+              : picture
+          }
+          alt={pokemon.name}
+          className="pokemon-pic w-100 d-block m-auto"
+        />
       </div>
 
       <div className="d-flex flex-column align-items-start">
-        <h1 className="text-capitalize pb-3">{pokemon.name}</h1>
-        <h2 className="text-center mb-2">Abilities:</h2>
+        <div className="text-capitalize pb-3">{pokemon.name}</div>
+        <div className="text-center mb-2">Abilities:</div>
         <ul className="d-flex justify-content-center mb-4">
-          {pokemon.abilities.map(item => <li key={item.ability.name} className="abilities bg-success mr-2 p-2 rounded font-weight-bold">{item.ability.name}</li>)}
+          {pokemon.abilities.map((item) => (
+            <li
+              key={item.ability.name}
+              className="abilities bg-success mr-2 p-2 rounded font-weight-bold"
+            >
+              {item.ability.name}
+            </li>
+          ))}
         </ul>
         <div className="d-flex flex-column align-items-start">
-          <h2 className="text-center mb-2">Stats:</h2>
+          <div className="text-center mb-2">Stats:</div>
           <ul className="d-flex flex-wrap">
-            {pokemon.stats.map(stats => (
-              <li key={stats.stat.name} className="stats mb-3 bg-primary mr-2 p-2 rounded font-weight-bold">
-                <span>
-                  {stats.stat.name}
-                  :
-                </span>
-                {' '}
-                {stats.base_stat}
-              </li>
-            ))}
+            <Chart
+              chartType="PieChart"
+              data={buildData(pokemon)}
+              options={pieOptions}
+              graph_id="PieChart"
+              width={"100%"}
+              height={"100%"}
+              legend_toggle
+            />
           </ul>
-          <button type="button" className="btn btn-secondary" onClick={goBackHandle}>Go back</button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={goBackHandle}
+          >
+            Go back
+          </button>
         </div>
       </div>
     </div>
@@ -45,10 +122,13 @@ const Pokemon = ({ pokemon }) => {
 
 Pokemon.defaultProps = {
   pokemon: {
-    name: 'pikachu',
+    name: "pikachu",
     abilities: [],
     stats: [],
-    sprites: { front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png' },
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    },
   },
 };
 
